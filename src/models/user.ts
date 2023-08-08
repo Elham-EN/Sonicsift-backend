1575f179d0f2da1f14abb36fccea544d531f6fa3
Elham-EN <ilhamrezaie@gmail.com>
2023-08-08 18:18:56 +1000

    created User model

---
+++ src/models/user.ts
@@ -0,0 +1,54 @@
+import { Model, ObjectId, Schema, model } from "mongoose";
+// Define user model type and structure
+interface UserDocument {
+  name: string;
+  email: string;
+  password: string;
+  verified: boolean; // to verify users
+  avatar?: { url: string; publicId: string };
+  // store authentication token of users, where users can
+  // signin inside the application from multiple devices
+  tokens: string[];
+  // users can have multiple favourites on their list
+  favorites: ObjectId[];
+  followers: ObjectId[];
+  followings: ObjectId[];
+}
+
+// Create user schema (structure the data shape)
+const userSchema = new Schema<UserDocument>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: Object,
+      url: String,
+      publicId: String,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    // Array of ObjectIds
+    favorites: [{ type: Schema.Types.ObjectId, ref: "Audio" }],
+    followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    followings: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    tokens: [String],
+  },
+  { timestamps: true }
+);
+
+export default model("User", userSchema) as Model<UserDocument>;
