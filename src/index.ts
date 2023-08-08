ac35a5c0a6ed8f7113fdbec0bc2447a6c85508bf
Elham-EN <ilhamrezaie@gmail.com>
2023-08-08 20:40:03 +1000

    created Auth router and post request to create user

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import "./db";
-
+import authRouter from "./routers/auth";
 const app = express();
+
+// Register middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/auth", authRouter);
 
 const PORT = process.env.PORT || 8989;
 
